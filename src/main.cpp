1ce3985bf6a51ecfb21757621efd3f855a45dd9b
m-lima <marcelowind@gmail.com>
2016-10-03 18:18:33 +0200

    Scanning devices

--- src/main.cpp
+++ src/main.cpp
@@ -2,15 +2,6 @@
 #include <vector>
 #include <string>
 
-/* #define CL_HPP_ENABLE_EXCEPTIONS */
-/* #define CL_HPP_TARGET_OPENCL_VERSION 200 */
-
-/* #ifdef __APPLE__ */
-/* #include <OpenCL/cl.h> */
-/* #else */
-/* #include <CL/cl.h> */
-/* #endif */
-
 #define __CL_ENABLE_EXCEPTIONS
 #include <cl.hpp>
 
@@ -24,6 +15,42 @@ int main() {
       std::cerr << "OpenCL platforms not found." << std::endl;
       return 1;
     }
+
+    cl::Context context;
+    std::vector<cl::Device> devices;
+    for (auto p = platforms.begin(); devices.empty() && p != platforms.end(); p++) {
+      std::vector<cl::Device> platformDevices;
+
+      try {
+        p->getDevices(CL_DEVICE_TYPE_GPU, &platformDevices);
+
+        for (auto device = platformDevices.begin(); devices.empty() && device != platformDevices.end(); device++) {
+          std::string ext = device->getInfo<CL_DEVICE_EXTENSIONS>();
+
+          if (ext.find("cl_khr_fp64") == std::string::npos) {
+            std::cout << "cl_khr_fp64" << std::endl;
+            if (ext.find("cl_amd_fp64") == std::string::npos) {
+              std::cout << "cl_amd_fp64" << std::endl;
+              std::cout << "Skipping: " << device->getInfo<CL_DEVICE_NAME>() << std::endl;
+              continue;
+            }
+          }
+
+          devices.push_back(*device);
+          context = cl::Context(devices);
+        }
+
+      } catch (...) {
+        devices.clear();
+      }
+    }
+
+    if (devices.empty()) {
+      std::cerr << "GPUs with double precision not found." << std::endl;
+      return 1;
+    }
+
+    std::cout << devices[0].getInfo<CL_DEVICE_NAME>() << std::endl;
 
   } catch (const cl::Error &err) {
     std::cerr
