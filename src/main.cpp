060ffa6675cabb7c4f03bec5ffd528e729055a00
m-lima <marcelowind@gmail.com>
2016-10-03 21:56:20 +0200

    Using boost

--- src/main.cpp
+++ src/main.cpp
@@ -1,50 +1,61 @@
 #include <iostream>
+#include <iomanip>
 #include <vector>
 #include <string>
+#include <boost/algorithm/string.hpp>
 
 #define __CL_ENABLE_EXCEPTIONS
 #include <cl.hpp>
 
 bool listALL() {
-	try {
-		std::vector<cl::Platform> platforms;
-		cl::Platform::get(&platforms);
+  std::cout << "Listing all platforms and devices.." << std::endl;
+  try {
+    std::vector<cl::Platform> platforms;
+    cl::Platform::get(&platforms);
+    std::string name;
 
-		if (platforms.empty()) {
-			std::cerr << "OpenCL platforms not found." << std::endl;
-			return false;
-		}
+    if (platforms.empty()) {
+      std::cerr << "OpenCL platforms not found." << std::endl;
+      return false;
+    }
 
-		cl::Context context;
-		std::vector<cl::Device> devices;
-		for (auto platform = platforms.begin(); platform != platforms.end(); platform++) {
-			std::cout << "Platform:\t\t\t" << platform->getInfo<CL_PLATFORM_NAME>() << std::endl;
-			std::vector<cl::Device> platformDevices;
+    cl::Context context;
+    std::vector<cl::Device> devices;
+    for (auto platform = platforms.begin(); platform != platforms.end(); platform++) {
+      name = platform->getInfo<CL_PLATFORM_NAME>();
+      boost::trim(name);
+      std::cout << std::setw(11) << "Platform:" << std::setw(5) << ' ' << name << std::endl;
+      std::vector<cl::Device> platformDevices;
 
-			try {
-				platform->getDevices(CL_DEVICE_TYPE_ALL, &platformDevices);
+      try {
+        platform->getDevices(CL_DEVICE_TYPE_ALL, &platformDevices);
 
-				for (auto device = platformDevices.begin(); device != platformDevices.end(); device++) {
-					std::cout << "\tDevice:\t\t\t" << device->getInfo<CL_DEVICE_NAME>() << std::endl;
-				}
-			}
-			catch (...) {
-			}
-		}
-	}
-	catch (const cl::Error &err) {
-		std::cerr
-			<< "OpenCL error: "
-			<< err.what() << '(' << err.err() << ')'
-			<< std::endl;
-		return false;
-	}
+        for (auto device = platformDevices.begin(); device != platformDevices.end(); device++) {
+          name = device->getInfo < CL_DEVICE_NAME>();
+          boost::trim(name);
+          std::cout << std::setw(11) << "Device:" << std::setw(5) << ' ' << name << std::endl;
+        }
+      }
+      catch (...) {
+      }
+    }
+  }
+  catch (const cl::Error &err) {
+    std::cerr
+      << "OpenCL error: "
+      << err.what() << '(' << err.err() << ')'
+      << std::endl;
+    return false;
+  }
 
-	return true;
+  return true;
 }
 
 int main() {
   listALL();
+  std::cout << std::endl;
+  std::cout << std::endl;
+
   try {
     std::vector<cl::Platform> platforms;
     cl::Platform::get(&platforms);
@@ -86,7 +97,9 @@ int main() {
     }
 
     for (auto device = devices.begin(); device != devices.end(); device++) {
-      std::cout << "Acceptable: " << device->getInfo<CL_DEVICE_NAME>() << std::endl;
+      std::string name = device->getInfo<CL_DEVICE_NAME>();
+      boost::trim(name);
+      std::cout << std::setw(11) << "Acceptable:" << std::setw(5) << ' ' << name << std::endl;
     }
 
   } catch (const cl::Error &err) {
