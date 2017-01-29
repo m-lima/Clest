6aa802e68331be28c9674b73b1a0be4a5c7c81fe
m-lima <marcelowind@gmail.com>
2017-01-29 21:17:48 +0100

    Clean up

--- src/cpp/cl/marching_program.hpp
+++ src/cpp/cl/marching_program.hpp
@@ -1,15 +1,15 @@
 #pragma once
 
-#include "clest_program.hpp"
 #include <fmt\format.h>
+
+#include "clest_program.hpp"
 
 namespace clest {
 
   class MarchingProgram : public ClestProgram {
   public:
     MarchingProgram(unsigned int iso) :
-      ClestProgram(fmt::format(" -D CONST_ISO={:d}",
-                               iso)) {}
+      ClestProgram(fmt::format(" -D CONST_ISO={:d}", iso)) {}
 
     inline const char * const path() const override {
       return "opencl/marching.cl";
--- src/opencl/lop.cl
+++
@@ -1,8 +0,0 @@
-//Block cell
-__kernel void lop(__global unsigned char * input,
-                  __global unsigned char * output,
-                  const unsigned int width
-) {
-  unsigned int index = get_global_id(0);
-
-}
